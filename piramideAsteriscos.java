ce82771c60c6a6cbfc302b8037bab38e41196d84
Ivanovichack <ivanthebest10@gmail.com>
2020-12-12 16:44:47 -0600

    Actualizado ciclo flor

--- piramideAsteriscos.java
+++ piramideAsteriscos.java
@@ -2,7 +2,10 @@ public class piramideAsteriscos{
 
     public static void main(String [] args){
         for(int i = 0; i < 11; i++){
-            System.out.println("*");
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
 
     }
