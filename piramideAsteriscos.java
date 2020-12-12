9eb9f167810fd6339f852bff03c2113a0ed29d8d
Ivanovichack <ivanthebest10@gmail.com>
2020-12-12 17:17:51 -0600

    agregue otro comentario

--- piramideAsteriscos.java
+++ piramideAsteriscos.java
@@ -2,7 +2,7 @@ public class piramideAsteriscos{
 
     public static void main(String [] args){
         for(int i = 0; i < 11; i++){//pisos que tendra la piramide
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++){//aumentando en cada ciclo
                 System.out.print("*");//asteriscos que tendra cada piso
             }
             System.out.println("");
